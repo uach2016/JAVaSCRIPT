465e02da965521c4677dd64807277c57b1fdcd17
uach2016 <uacaminoherrera@gmail.com>
2016-07-19 11:27:04 +0100

    Add files via upload

---
+++ arrange_array.js
@@ -0,0 +1,9 @@
+
+//It receives an array with numbers and letters mixed and returns it with its items arrange:
+//First goes the numbers and then the letters. This comes by defuelt using .sort
+function arrangeElements( array ) {
+	array = array.sort();
+	return array;
+}
+	
+	
